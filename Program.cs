d6a4c6249192785a15443429af9d127b4dad0d40
Ai Uragaki <ai.uragaki.nz@gmail.com>
2021-06-08 19:49:19 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FindGreaterOfTowNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+
+            Console.WriteLine("Please enter first Number");
+            number1 = Convert.ToInt32(Console.ReadLine()); // This statement is converting the string into int
+
+            Console.WriteLine("Please enter second Number");
+            number2 = Convert.ToInt32(Console.ReadLine()); // This statement is converting the strinf into int
+
+                if(number1 > number2)
+            {
+                Console.WriteLine(number1.ToString()+ " is greater number "+ number2.ToString());
+            }
+
+                else
+            {
+                Console.WriteLine("number2 is greater number");
+            }
+
+        }
+    }
+}
